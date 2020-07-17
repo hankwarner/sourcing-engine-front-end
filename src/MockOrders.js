284463c4033f4d5ce08010b59ce383416c31106d
Austin Blaine <austin.blaine@supply.com>
2020-07-17 17:11:23 -0400

    working on single order dialog boxes

---
+++ src/MockOrders.js
@@ -0,0 +1,173 @@
+export default [
+    {
+        "ordernum": "123456",
+        "numsources": "12",
+        "status": "open",
+        "claimedby": "",
+        "subtotal": 4568.00,
+        "tax": 100.00,
+        "shipping": 99.00,
+        "total": 4767.00,
+        "addresses": {
+            "billing": {
+                "fname": "Michael",
+                "lname": "Owens",
+                "street1": "2305 Vineyard Court",
+                "street2": "",
+                "city": "Villa Rica",
+                "state": "GA",
+                "postalcode": "30180"
+            },
+            "shipping": {
+                "fname": "Michael",
+                "lname": "Owens",
+                "street1": "2305 Vineyard Court",
+                "street2": "",
+                "city": "Villa Rica",
+                "state": "GA",
+                "postalcode": "30180"
+            }
+        },
+        "sourcing": {
+            "25478": {
+                "name": "Fort Payne",
+                "items": {
+                    "3249817": {
+                        "qty": 4,
+                        "eachprice": 200.00
+                    },
+                    "23547": {
+                        "qty": 4,
+                        "eachprice": 200.00
+                    }
+                }
+            },
+            "846567": {
+                "name": "Parris",
+                "items": {
+                    "3524485": {
+                        "qty": 4,
+                        "eachprice": 400.00
+                    },
+                    "21448": {
+                        "qty": 4,
+                        "eachprice": 50.00
+                    }
+                }
+            }
+        }
+    },
+    {
+        "ordernum": "78946",
+        "numsources": "1",
+        "status": "claimed",
+        "claimedby": "Michael Owens",
+        "subtotal": 4568.00,
+        "tax": 100.00,
+        "shipping": 99.00,
+        "total": 4767.00,
+        "addresses": {
+            "billing": {
+                "fname": "Michael",
+                "lname": "Owens",
+                "street1": "2305 Vineyard Court",
+                "street2": "",
+                "city": "Villa Rica",
+                "state": "GA",
+                "postalcode": "30180"
+            },
+            "shipping": {
+                "fname": "Michael",
+                "lname": "Owens",
+                "street1": "2305 Vineyard Court",
+                "street2": "",
+                "city": "Villa Rica",
+                "state": "GA",
+                "postalcode": "30180"
+            }
+        },
+        "sourcing": {
+            "25478": {
+                "name": "Fort Payne",
+                "items": {
+                    "3249817": {
+                        "qty": 4,
+                        "eachprice": 200.00
+                    },
+                    "23547": {
+                        "qty": 4,
+                        "eachprice": 200.00
+                    }
+                }
+            }
+        }
+    },
+    {
+        "ordernum": "897435",
+        "numsources": "4",
+        "status": "open",
+        "claimedby": "",
+        "subtotal": 4568.00,
+        "tax": 100.00,
+        "shipping": 99.00,
+        "total": 4767.00,
+        "addresses": {
+            "billing": {
+                "fname": "Michael",
+                "lname": "Owens",
+                "street1": "2305 Vineyard Court",
+                "street2": "",
+                "city": "Villa Rica",
+                "state": "GA",
+                "postalcode": "30180"
+            },
+            "shipping": {
+                "fname": "Michael",
+                "lname": "Owens",
+                "street1": "2305 Vineyard Court",
+                "street2": "",
+                "city": "Villa Rica",
+                "state": "GA",
+                "postalcode": "30180"
+            }
+        },
+        "sourcing": {
+            "65547": {
+                "name": "WhatyaDoin",
+                "items": {
+                    "132457": {
+                        "qty": 1,
+                        "eachprice": 150.00
+                    }
+                }
+            },
+            "6224786": {
+                "name": "Atlanta",
+                "items": {
+                    "6548465": {
+                        "qty": 1,
+                        "eachprice": 50.00
+                    }
+                }
+            },
+            "58876": {
+                "name": "WhatyaDoin",
+                "items": {
+                    "132457": {
+                        "qty": 1,
+                        "eachprice": 150.00
+                    }
+                }
+            },
+            "9645673": {
+                "name": "Atlanta",
+                "items": {
+                    "6548465": {
+                        "qty": 1,
+                        "eachprice": 50.00
+                    }
+                }
+            }
+        }
+    }
+]
---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from '../Header/Header';
+import MainContent from '../MainContent/MainContent';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
