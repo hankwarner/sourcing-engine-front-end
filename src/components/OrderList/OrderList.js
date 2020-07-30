5aa0b493201ae5070ce835b5fb2163e9ea5328c0
Austin Blaine <austin.blaine@supply.com>
2020-07-30 14:35:18 -0400

    install axios - middle of trying to get a fetch to work

--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import MockOrders from '../../MockOrders'
 import { makeStyles } from '@material-ui/core/styles';
 import SingleOrder from '../SingleOrder/SingleOrder'
+// import getOrders from '../../server/endpoints'
 
 const useStyles = makeStyles(() => ({
     orderList: {
@@ -12,10 +13,20 @@ const useStyles = makeStyles(() => ({
         paddingTop: 30,
         marginBottom:40
     }
-  }));
+}));
+
 
 
 export default function OrderList() {
+
+    // useEffect(async () => {
+    //     await fetch('https://fergusonsourcingengine.azurewebsites.net/api/manual-orders?code=mnYhnVWLaPFZk4WsoC1tCHqANea0XlCdisYa5roo0FZaC/jX6E72Cw==')
+    //     .then(response => response.json())
+    //     .then(json => console.log(json))
+     
+    //     // setData(result.data);
+    //   });
+
     const classes = useStyles();
 
     const openOrders = MockOrders.filter(order => order.claimed === false)
---
+++ src/server/endpoints.js
@@ -0,0 +1,11 @@
+// Get a List of All orders: 
+
+// const getOrders = `fergusonsourcingengine.azurewebsites.net/api/manual-orders?code=mnYhnVWLaPFZk4WsoC1tCHqANea0XlCdisYa5roo0FZaC/jX6E72Cw==`
+
+// Claim an order: {id} = atgOrderId 
+
+// const claimOrder = `fergusonsourcingengine.azurewebsites.net/api/order/claim/${id}?code=hmYQxSz505g1dPCNFtaBPhhjeMFQRNxlYAh91owaJGVDcbnpQ4b4hw==`
+
+// Release an order: {id} = atgOrderId  
+
+// const releaseOrder = `fergusonsourcingengine.azurewebsites.net/api/order/release/${id}?code=O94pZJNzX07aaGJaAfLayaSPl96XF9qRaAajP41Az5wiofHFD4C7zw==`
