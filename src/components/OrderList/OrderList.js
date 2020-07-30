aab1ad7db5903dc5b9212f7d73a1b121ac557d7e
Michael Owens <m.owens@supply.com>
2020-07-30 15:11:53 -0400

    CORS error...for days...it might be related to local host

--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import MockOrders from '../../MockOrders'
 import { makeStyles } from '@material-ui/core/styles';
 import SingleOrder from '../SingleOrder/SingleOrder'
-// import getOrders from '../../server/endpoints'
+
+import Axios from 'axios'
 
 const useStyles = makeStyles(() => ({
     orderList: {
@@ -15,17 +16,24 @@ const useStyles = makeStyles(() => ({
     }
 }));
 
+Axios.get('https://fergusonsourcingengine.azurewebsites.net/api/manual-orders', {
+    params: {
+      code: 'mnYhnVWLaPFZk4WsoC1tCHqANea0XlCdisYa5roo0FZaC/jX6E72Cw=='
+    }
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
 
 
 export default function OrderList() {
 
-    // useEffect(async () => {
-    //     await fetch('https://fergusonsourcingengine.azurewebsites.net/api/manual-orders?code=mnYhnVWLaPFZk4WsoC1tCHqANea0XlCdisYa5roo0FZaC/jX6E72Cw==')
-    //     .then(response => response.json())
-    //     .then(json => console.log(json))
-     
-    //     // setData(result.data);
-    //   });
 
     const classes = useStyles();
 
