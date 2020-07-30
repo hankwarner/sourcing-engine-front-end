3ab566a56cffaba8dfb5443b5bf6d7a31664d49a
Michael Owens <m.owens@supply.com>
2020-07-30 17:40:48 -0400

    trying something else live that probably won't work

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'; 
 import Header from '../Header/Header';
 import MainContent from '../MainContent/MainContent';
 
@@ -6,18 +6,36 @@ import { ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import theme from './theme'
 
-
+import axios from 'axios'
 
 function App() {
+
+  const [orderData, setOrderData] = useState({});
+
+  useEffect(() => {
+    getOrdersFromAPI();
+  }, []);
+
+  const getOrdersFromAPI = async () => {
+    const response = await axios.get("https://fergusonsourcingengine.azurewebsites.net/api/manual-orders?code=mnYhnVWLaPFZk4WsoC1tCHqANea0XlCdisYa5roo0FZaC/jX6E72Cw==");
+    setOrderData(response.data)
+  };
+  
+  console.log(orderData);
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>       
     <CssBaseline />
       <div className="App">
         <Header />
         <MainContent />
       </div>
       </ThemeProvider>
+
+      
   );
+
+ 
 }
 
 export default App;
--- src/components/OrderList/OrderList.js
+++ src/components/OrderList/OrderList.js
@@ -3,7 +3,7 @@ import MockOrders from '../../MockOrders'
 import { makeStyles } from '@material-ui/core/styles';
 import SingleOrder from '../SingleOrder/SingleOrder'
 
-import Axios from 'axios'
+
 
 const useStyles = makeStyles(() => ({
     orderList: {
@@ -16,20 +16,6 @@ const useStyles = makeStyles(() => ({
     }
 }));
 
-Axios.get('https://fergusonsourcingengine.azurewebsites.net/api/manual-orders', {
-    params: {
-      code: 'mnYhnVWLaPFZk4WsoC1tCHqANea0XlCdisYa5roo0FZaC/jX6E72Cw=='
-    }
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
 
 
 export default function OrderList() {
