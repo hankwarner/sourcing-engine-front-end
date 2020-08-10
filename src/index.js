8f66de83fc5d084a325aa311662559d616b1f3f2
Austin Blaine <austin.blaine@supply.com>
2020-08-10 12:21:39 -0400

    starting to setup graphql

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'https://graphql-pokemon.now.sh/'
+})
+
+const client = new ApolloClient({
+  cache,
+  link
+})
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloClient client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloClient>,
   document.getElementById('root')
 );
 
