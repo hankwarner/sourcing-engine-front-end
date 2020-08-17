85eae0dc33b5778b9822c2565808d443b9110ede
Austin Blaine <austin.blaine@supply.com>
2020-08-17 14:56:47 -0400

    query written in full

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -5,6 +5,54 @@ export const GET_ORDERS = gql`
     getOrders {
       atgOrderId
       custAccountId
+      customerId
+      customerName
+      orderSubmitDate
+      orderRequiredDate
+      sellWhse
+      sourcingMessage
+      shipFrom
+      claimed
+      orderComplete
+      paymentOnAccount {
+        payment {
+            cardType
+            address1
+            address2
+            city
+            state
+            zip
+        }
+    }
+    shipping {
+      shipViaCode
+      price 
+      shipTo {
+        name
+        address1
+        address2
+        city
+        country
+        state
+        zip
+        shipInstructionsPhoneNumberAreaDialing
+        shipInstructionsPhoneNumberDialNumber
+      }
+    }
+    sourcing { 
+      shipFrom
+      sourceComplete
+      items {
+        lineItemId
+        unitPriceCode
+        description
+        quantity
+        sourcingMessage
+        masterProdId
+        itemComplete
+      }
+  }     
+      sourceSystem
     }
   }
 `;
