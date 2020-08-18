6fa9f619e016af419d17f2447c2f5fcc2501ded5
Austin Blaine <austin.blaine@supply.com>
2020-08-18 16:46:37 -0400

    working on building out api with code params

--- graphql/apis.js
+++ graphql/apis.js
@@ -8,41 +8,53 @@ class FergusonSourcingEngineAPI extends RESTDataSource {
 
   willSendRequest(request) {
     request.headers.set('Content-Type', 'application/json; charset=utf-8');
-    request.params.set(
-      'code',
-      'mnYhnVWLaPFZk4WsoC1tCHqANea0XlCdisYa5roo0FZaC/jX6E72Cw=='
-    );
+    if(this.getOrders){
+      request.params.set(
+        'code',
+        'mnYhnVWLaPFZk4WsoC1tCHqANea0XlCdisYa5roo0FZaC/jX6E72Cw=='
+      );
+    } else if(this.checkClaim) {
+      request.params.set(
+        'code',
+        'akU/NdJzya8EPdFGYD/NJtJ/VJRYDgGGI/nco5ujNyDdcrCHQAhWWg=='
+      );
+    } else if(this.claimOrder) {
+      request.params.set(
+        'code',
+        'hmYQxSz505g1dPCNFtaBPhhjeMFQRNxlYAh91owaJGVDcbnpQ4b4hw=='
+      );
+    } else if(this.releaseOrder) {
+      request.params.set(
+        'code',
+        'O94pZJNzX07aaGJaAfLayaSPl96XF9qRaAajP41Az5wiofHFD4C7zw=='
+      );
+    } else if(this.orderComplete) {
+      request.params.set(
+        'code',
+        '9cs9ToHl8eWGhKttxxosn0dLLIdqLZofJem1D4RASPW8o/7S9BIkeQ=='
+      );
+    }
   }
 
   async getOrders() {
     return this.get(`${this.baseURL}/manual-orders`);
   }
 
-  // code = 'mnYhnVWLaPFZk4WsoC1tCHqANea0XlCdisYa5roo0FZaC/jX6E72Cw=='
+  async checkClaim(id) {
+    return this.post(`${this.baseURL}/order/is-claimed/${id}`)
+  }
 
-  // async checkClaim(id) {
-  //   return this.post(`${this.baseURL}/order/is-claimed/${id}`)
-  // }
+  async claimOrder(id) {
+    return this.post(`${this.baseURL}/order/claim/${id}`);
+  }
 
-  // code = 'akU/NdJzya8EPdFGYD/NJtJ/VJRYDgGGI/nco5ujNyDdcrCHQAhWWg=='
+  async releaseOrder(id) {
+    return this.post(`${this.baseURL}/order/release/${id}`);
+  }
 
-  // async claimOrder(id) {
-  //   return this.post(`${this.baseURL}/order/claim/${id}`);
-  // }
-
-  // code = 'hmYQxSz505g1dPCNFtaBPhhjeMFQRNxlYAh91owaJGVDcbnpQ4b4hw=='
-
-  // async releaseOrder(id) {
-  //   return this.post(`${this.baseURL}/order/release/${id}`);
-  // }
-
-  // code = 'O94pZJNzX07aaGJaAfLayaSPl96XF9qRaAajP41Az5wiofHFD4C7zw=='
-
-  // async orderComplete(id) {
-  //   return this.post(`${this.baseURL}/order/complete/${id}`);
-  // }
-
-  // code = '9cs9ToHl8eWGhKttxxosn0dLLIdqLZofJem1D4RASPW8o/7S9BIkeQ=='
+  async orderComplete(id) {
+    return this.post(`${this.baseURL}/order/complete/${id}`);
+  }
 }
 
 module.exports = {
