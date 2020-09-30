81a531c864cd80154b7ff67724216cf95f5a750f
Lisa Dean <lisa@lisadean.net>
2020-09-30 15:02:10 -0400

    added phone number back into story

--- src/components/OrderAddresses/OrderAddresses.stories.js
+++ src/components/OrderAddresses/OrderAddresses.stories.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import OrderAddresses from './OrderAddresses';
 
-const mockData = {
+const phone = '(315)729-5356';
+const address = {
 	name: 'John Doe',
 	address1: '123 Main St',
 	address2: 'Suite 100',
 	city: 'Atlanta',
 	state: 'GA',
 	zip: '30303',
-	phone: '(315)7295356',
-
 };
-const { state, ...mockDataWithoutState } = mockData;
-const {
-	phone: phone2,
-	...mockDataWithoutPhoneNumber
-} = mockData;
+const { state, ...addressWithoutState } = address;
 
 // This default export determines where you story goes in the story list
 export default {
@@ -23,21 +18,23 @@ export default {
 	component: OrderAddresses,
 };
 
-const Template = (args) => <OrderAddresses shipTo={{ ...args }} />;
+const Template = (args) => (
+	<OrderAddresses shipTo={{ ...args.address }} phone={args.phone} />
+);
 
 export const defaultComponent = Template.bind({});
-defaultComponent.args = { ...mockData };
+defaultComponent.args = { address, phone };
 
 export const withoutState = Template.bind({});
-withoutState.args = { ...mockDataWithoutState };
+withoutState.args = { address: addressWithoutState, phone };
 
 export const withoutPhoneNumber = Template.bind({});
-withoutPhoneNumber.args = { ...mockDataWithoutPhoneNumber };
+withoutPhoneNumber.args = { address, phone: null };
 
 export const withoutAnyAddressParts = (args) => {
 	return (
 		<div>
-			<OrderAddresses shipTo={{ ...args }} phone={''} />
+			<OrderAddresses shipTo={{ ...args.address }} phone={args.phone} />
 			<span role="img" aria-label="info" style={{ paddingRight: '1em' }}>
 				ℹ️
 			</span>
@@ -49,10 +46,13 @@ export const withoutAnyAddressParts = (args) => {
 	);
 };
 withoutAnyAddressParts.args = {
-	name: '',
-	address1: '',
-	address2: '',
-	city: '',
-	state: '',
-	zip: '',
+	address: {
+		name: null,
+		address1: null,
+		address2: null,
+		city: null,
+		state: null,
+		zip: null,
+	},
+	phone: null,
 };
